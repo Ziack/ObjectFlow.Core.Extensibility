9b4ad9dde7d538537460a5ecc8e0d353c94a506d
canro91 <canro91@hotmail.com>
2017-06-05 22:49:44 -0500

    Add sample project

---
+++ Sample/CreateDoors.cs
@@ -0,0 +1,14 @@
+﻿using ObjectFlow.Core.Extensibility.Framework;
+
+namespace ObjectFlow.Core.Extensibility.Sample
+{
+    public class CreateDoors : StepOperation<Car>
+    {
+        public override Car Execute(Car data)
+        {
+            data.NumberOfDoors = 5;
+
+            return data;
+        }
+    }
+}
